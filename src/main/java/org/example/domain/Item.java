1832f5ff103d0483ac31f223dd8c80ef68f95630
Beomchu <izekfzha@gmail.com>
2023-01-29 05:25:10 +0900

    jpa-review ver1.1

--- src/main/java/org/example/domain/Item.java
+++ src/main/java/org/example/domain/Item.java
@@ -1,7 +1,47 @@
 package org.example.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
--- src/main/java/org/example/domain/Order.java
+++ src/main/java/org/example/domain/Order.java
@@ -10,8 +10,42 @@ public class Order {
     @Id
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    @Column(name = "member_id")
     private Long memberId;
 
+    //강의에서 1.8 이후로는 데이터베이스에 적용 가능하다고 봤음 강의 확인 후 수정
     private LocalDateTime orderDate;
 
     // ORDINAL 설정시에 데이터베이스에 0,1 등으로 저장되는데 enum순서 변경 혹은 추가시 위험할 수 있음
